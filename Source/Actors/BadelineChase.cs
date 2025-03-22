9958d9726650899e3d811674a65d6b4fae9cdcf0
Cat Aulucya <cat@aulucya.gay>
2025-03-22 11:19:32 -0700

    Add Time to Badeline, as required by Foster 0.2.1
    Co-authored-by: Luna Aulucya <https://github.com/InLieuOfLuna>
    Signed-off-by: Cat Aulucya <cat@aulucya.gay>

--- Source/Actors/BadelineChase.cs
+++ Source/Actors/BadelineChase.cs
@@ -61,7 +61,7 @@ public class BadelineChase : Actor, IHaveModels, IPickup, ICastPointShadow
 			hair.Flags = Model.Flags;
 			hair.Forward = -new Vec3(Facing, 0);
 			hair.Materials[0].Effects = 0;
-			hair.Update(hairMatrix);
+			hair.Update(Time, hairMatrix);
 		}
 
     }
